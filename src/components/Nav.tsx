85a322331c006ae6a258af54ea60901648702d61
kutho53 <kurtisthomas53@gmail.com>
2025-02-09 00:30:12 -0500

    .env fix

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -6,7 +6,7 @@ const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
     <nav>
-      <a href='./CandidateSearch.tsx'>Home </a>
+      <a href='./CandidateSearch.tsx'>Home  </a>
       <a href='./SavedCandidates.tsx'>Potential Candidates</a>
     </nav>
   )
